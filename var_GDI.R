defb55754ba9fb32df8a35be72eab556d119dbde
DavidCornejoArias <davidcornejoarias@gmail.com>
2023-04-13 16:44:34 -0400

    deleted ine01 in join GDI
    Some surveys do not include it so couldn't make the merge

--- var_GDI.R
+++ var_GDI.R
@@ -44,6 +44,6 @@ data_gdi <- data_filt %>%
                                         edad_ci >=61 & edad_ci <= 75 ~ "rango_61_75", 
                                         edad_ci >=76 & edad_ci <= 90 ~ "rango_76_90",
                                         edad_ci>=91 ~ "rango_91_mas")) %>% 
-  select(disability, afroind_ci, ethnicity, recortes_poblacion, ine01, region_BID_c, pais_c, estrato_ci, zona_c, relacion_ci, idh_ch, factor_ch, factor_ci, 
+  select(disability, afroind_ci, ethnicity, recortes_poblacion, ine01, region_BID_c, pais_c, ine01, estrato_ci, zona_c, relacion_ci, idh_ch, factor_ch, factor_ci, 
          idp_ci)
 }
